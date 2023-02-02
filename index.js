3685017cd09df109c549b0be1ea8a798e6935701
AlexxDv <alexxdimov@gmail.com>
2023-02-02 17:01:05 +0100

    home page added

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser())
 
+app.get('/', (req, res) => {
+    res.send(`
+    <h1>Hello Ivan</h1>
+
+    <p><a href="/login">Login</a></p>
+    <p><a href="/profile">Profile</a></p>
+
+    `)
+})
+
 app.get('/login', (req, res) => {
     res.send(`
     <form method="POST">
@@ -25,7 +35,11 @@ app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
     if (username == "Ivan" && password == "peti") {
-
+        const authData = {
+            username: Ivan,
+        }
+        res.cookie('auth', JSON.stringify(authData));
+        res.redirect('/');
     }
 
     res.status(401).end()
